f6dbbd00c291123c3081da214ae406144dab8a5a
JWaldburger <49599965+JWaldburger@users.noreply.github.com>
2021-04-03 11:23:15 -0500

    Added my GitHub born on date.

--- src/GitHubBornOn.js
+++ src/GitHubBornOn.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import { API } from 'aws-amplify';
+import React
+    ,{
+        useState
+        , useEffect
+    } from 'react';
 
 export const GitHubBornOn = () => {
-    return (
+
+    const getData = async () => {
+        try {
+            const data = await API.get("api222a5355", "/born");
+            showInfo(data.borninfo);
+        }
+        catch(err) {
+            console.error(err);
+        }
+    };
+
+    useEffect(
+        () => {
+            getData();
+        }
+        , []
+    );
+
+    const [bornInfo, showInfo] = useState({});
+
+    return(
         <h2>
-            my github name goes here - my git hub created_at goes here
-            </h2>
+            {bornInfo.login} - {bornInfo.created_at}
+        </h2>
     );
 };
